eef7dae5fb5c08c525f6e8ecf69381af15180b45
Martijn DevNull <martijn@plebian.nl>
2014-10-06 12:52:19 +0200

    Working with the new icons

--- sassytray.py
+++ sassytray.py
@@ -5,21 +5,26 @@ import gtk, gobject, os
 
 TIMER = 2
 DEBUG = True
+LAST_STATUS = ""
 
 class CPUTimer:
     def __init__(self, timeout):
         self.tray = gtk.StatusIcon()
         self.tray.set_from_stock(gtk.STOCK_ABOUT)
-        # register a  timer
         gobject.timeout_add_seconds(timeout, self.timer_callback)
         self.timer_callback()
 
     def timer_callback(self):
+        global LAST_STATUS
         cpu_temp = self.get_CPU_Heat()
         self.debug('update CPU: ' + cpu_temp)
         icon_name = self.get_icon_name(int(cpu_temp))
         self.tray.set_tooltip(('CPU: ' + cpu_temp))
-        self.tray.set_from_file("icons/" + icon_name + ".svg")
+        self.debug("Setting LAST_STATUS: " + LAST_STATUS + " to " + icon_name)
+        if LAST_STATUS != icon_name:
+            self.debug("LAST_STATUS does not match icon_name, setting new icon")
+            self.tray.set_from_file("icons/" + icon_name + ".svg")
+        LAST_STATUS = icon_name
         return True
 
     def get_CPU_Heat(self):
@@ -27,13 +32,15 @@ class CPUTimer:
 
     def get_icon_name(self, heat):
         if heat < 30:
-            return "cold_cpu"
+            return "1"
         if heat < 50:
-            return "med_cpu"
+            return "2"
         if heat < 55:
-            return "hot_cpu"
+            return "3"
         if heat < 65:
-            return "flame_cpu"
+            return "4"
+        if heat < 70:
+            return "5"
         else:
             return "didn't catogorize heat"
 
@@ -41,6 +48,5 @@ class CPUTimer:
         if DEBUG is True:
             print mesg
 if __name__ == '__main__':
-    # The update interval for CPUTimer
     timer = CPUTimer(TIMER) 
     gtk.main()
